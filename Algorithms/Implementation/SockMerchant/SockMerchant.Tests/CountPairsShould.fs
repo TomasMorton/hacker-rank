f45190c2cfcfd6b40f7defd64c02855331022204
Tomas Morton <tomas@ayudasystems.com>
2019-02-17 16:04:21 -0500

    Format test file.

--- Algorithms/Implementation/SockMerchant/SockMerchant.Tests/CountPairsShould.fs
+++ Algorithms/Implementation/SockMerchant/SockMerchant.Tests/CountPairsShould.fs
@@ -5,47 +5,47 @@ open SockMerchant
 
 type TestColor = Red = 1 | Blue = 2
 
-let createSock (color: TestColor) =
+let createSock (color : TestColor) =
     let colorNumber = int color
     { SockCounter.Sock.Color = colorNumber }
-    
-    
+
+
 [<Fact>]
-let ``Have no pairs when there are no socks`` () =
+let ``Have no pairs when there are no socks``() =
     let socks = []
-    
+
     let numberOfPairs = SockCounter.countPairs socks
-    
+
     Assert.Equal(0, numberOfPairs)
 
 [<Fact>]
-let ``Have no pairs when there is one sock`` () =
+let ``Have no pairs when there is one sock``() =
     let socks = [ createSock TestColor.Red ]
-    
+
     let numberOfPairs = SockCounter.countPairs socks
-    
+
     Assert.Equal(0, numberOfPairs)
 
 [<Fact>]
-let ``Have no pairs when the socks are different colors`` () =
-    let socks = [ createSock TestColor.Red; createSock TestColor.Blue]
-    
+let ``Have no pairs when the socks are different colors``() =
+    let socks = [ createSock TestColor.Red; createSock TestColor.Blue ]
+
     let numberOfPairs = SockCounter.countPairs socks
-    
+
     Assert.Equal(0, numberOfPairs)
 
 [<Fact>]
-let ``Have one pair when the socks are the same color`` () =
-    let socks = [ createSock TestColor.Red; createSock TestColor.Red]
-    
+let ``Have one pair when the socks are the same color``() =
+    let socks = [ createSock TestColor.Red; createSock TestColor.Red ]
+
     let numberOfPairs = SockCounter.countPairs socks
-    
+
     Assert.Equal(1, numberOfPairs)
-    
+
 [<Fact>]
-let ``Have two pairs when there are 4 socks of the same color`` () =
+let ``Have two pairs when there are 4 socks of the same color``() =
     let socks = Seq.replicate 4 <| createSock TestColor.Blue
-    
+
     let numberOfPairs = SockCounter.countPairs socks
-    
+
     Assert.Equal(2, numberOfPairs)

b1e14aeb9c2d58bc24cb91f1f23a61bcb4c78e53
Tomas Morton <tomas@ayudasystems.com>
2019-02-17 14:46:17 -0500

    Implement solution

--- Algorithms/Implementation/SockMerchant/SockMerchant/SockCounter.fs
+++ Algorithms/Implementation/SockMerchant/SockMerchant/SockCounter.fs
@@ -1,7 +1,18 @@
 namespace SockMerchant
-    
+
 module SockCounter =
-    type Sock = | Color of int
-    
-    let countPairs (socks: seq<Sock>) =
-        socks |> Seq.length
+    type Sock = Color of int
+
+    let private groupSocksByColor socks =
+        socks
+        |> Seq.countBy (fun sock -> sock)
+
+    let private countPairsInGroup totalSocks =
+        totalSocks / 2
+
+    let countPairs (socks : seq<Sock>) =
+        socks
+        |> groupSocksByColor
+        |> Seq.map snd
+        |> Seq.map countPairsInGroup
+        |> Seq.sum

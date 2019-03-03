a39f1c5f984582143ae0dfb8112bd36cbe7dd5e2
Tomas Morton <tomas@ayudasystems.com>
2019-03-02 20:13:40 -0500

    Formatting for SockMerchant

--- Algorithms/Implementation/SockMerchant/SockMerchant.Tests/TestCases.fs
+++ Algorithms/Implementation/SockMerchant/SockMerchant.Tests/TestCases.fs
@@ -7,14 +7,16 @@ open TestCases
 type SockMerchantTestCase = { Socks : seq<SockMerchant.SockCounter.Sock>; ExpectedNumberOfPairs : int }
 
 let mapTestCase (testCase : TestCaseReader.TestCase) =
-        let socks = testCase.Input.[1].Trim().Split([|' '|])
-                    |> Seq.map (function color -> { SockCounter.Sock.Color = int color })
-        let numberOfPairs = testCase.ExpectedOutput.[0] |> int     
-        { Socks = socks; ExpectedNumberOfPairs = numberOfPairs }
+    let socks =
+        testCase.Input.[1].Trim().Split([|' '|])
+        |> Seq.map (function color -> { SockCounter.Sock.Color = int color })
+        
+    let numberOfPairs = testCase.ExpectedOutput.[0] |> int     
+    { Socks = socks; ExpectedNumberOfPairs = numberOfPairs }
     
 let executeTestCase mappedTestCase =
     printfn "Executing test case"
-    printfn "Expect number of pairs: %d" mappedTestCase.ExpectedNumberOfPairs
+    printfn "Expected number of pairs: %d" mappedTestCase.ExpectedNumberOfPairs
     printfn "Socks: %s" <| System.String.Join(", ", mappedTestCase.Socks)
     
     let numberOfPairs = SockCounter.countPairs mappedTestCase.Socks
